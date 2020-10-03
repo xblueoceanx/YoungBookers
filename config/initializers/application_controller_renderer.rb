a3e159eacfbd270dc4f89a56560d20e7a7173356
xblueoceanx <ginnnokizuna@yahoo.co.jp>
2020-10-03 14:10:11 +0900

    [update]view/css

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'cf0e639e802296bccc493d418872abbc1a41cf45e4815d077311e9fb6bec021a10966d23e0a6d02de9f47512c3a1b6bec26466386fe1dbf95e46115bc6e57510'
